62ecfcb676e0c3f489fd70f4a54fb6063c774e12
Ibrahim Dedeic <idedeic1@etf.unsa.ba>
2018-12-12 14:05:56 +0100

    2

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,13 +6,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+
+import static com.sun.javafx.scene.control.skin.Utils.getResource;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Pretraga datoteka");
-        primaryStage.setScene(new Scene(root, 450, 400));
+        Scene temp = new Scene(root, 450,400);
+        primaryStage.setScene(temp);
+        temp.getStylesheets().clear();
+        File f = new File("izgled.css");
+        temp.getStylesheets().add("file:///" + f.getAbsolutePath().replace("\\", "/"));
         primaryStage.show();
         primaryStage.setResizable(false);
     }
